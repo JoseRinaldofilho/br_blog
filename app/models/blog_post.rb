0ff1ab8bc6724bf9705ea6d74b650c8837d3ef90
jose.rinaldodev <jose.rinaldodev@gmail.com>
2022-03-24 00:56:31 -0300

    colocando as validaçoes validatesd

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -1,6 +1,10 @@
 class BlogPost < ApplicationRecord
     # validando o campo nome e idade campo obrigatorio
-    validates :nome, :idade , presence: true
-    validates :nome, length: {minimum: 2, maximum: 50}
-    validates :idade, length: {minimum: 1,maximum: 2}
+  validates :nome, :idade , presence: true
+  validates :nome, length: {minimum: 2, maximum: 50}
+  validates :idade, length: {minimum: 1,maximum: 2}
+
+  before_save do
+    self.nome = self.nome + " seja bem vindo"
+  end
 end
